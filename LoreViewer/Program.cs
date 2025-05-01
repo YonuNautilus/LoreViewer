53b85f5823d9b09a7b44de77a45df74b773df935
Jacob Baker <jacobmbaker@me.com>
2025-05-01 13:15:05 -0400

    a

--- LoreViewer/Program.cs
+++ LoreViewer/Program.cs
@@ -13,6 +13,9 @@ namespace LoreViewer
     public static void Main(string[] args)
     {
       BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
+    // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
